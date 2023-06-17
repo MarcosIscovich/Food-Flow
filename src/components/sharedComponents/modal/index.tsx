8aa7da95b6315356dc64b480f1aabef0e863228e
Esteban Giordano <86128230+egsta@users.noreply.github.com>
2023-06-17 18:21:45 -0300

    inicio crud

---
+++ src/components/sharedComponents/modal/index.tsx
@@ -0,0 +1,35 @@
+import { type PropFunction, Slot, component$ } from "@builder.io/qwik";
+
+interface ComponentProps {
+  show: boolean;
+  title: string;
+  size?: string;
+  onClose$: PropFunction<() => void>;
+}
+
+export const Modal = component$<ComponentProps>((props) => {
+  const { show, title, onClose$, size: _size } = props;
+
+  const sizeModal = _size
+    ? _size.length > 0
+      ? "lg:max-w-" + _size
+      : "lg:max-w-2xl"
+    : "lg:max-w-2xl";
+
+  return (
+    <div class={show ? "modal modal-middle modal-open" : "modal modal-middle"}>
+      <div class={`modal-box w-full ${sizeModal}`}>
+        <h3 class="font-bold text-lg">{title}</h3>
+        <div class="py-4">
+          <Slot />
+        </div>
+
+        <div class="modal-action">
+          <label for="my-modal-6" class="btn" onClick$={onClose$}>
+            Cerrar
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+});
---
+++ src/components/sharedComponents/utils/toast.component.tsx
@@ -0,0 +1,50 @@
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
+
+interface parametros {
+  msg: string;
+  type: string;
+  show: boolean;
+  onFinish: () => boolean;
+}
+
+export const Toast = component$<parametros>((props) => {
+  const { msg, type, show } = props;
+
+  let typeClass = "alert alert-info";
+  if (type == "error") {
+    typeClass = "alert alert-danger";
+  }
+  if (type == "success") {
+    typeClass = "alert alert-success";
+  }
+
+  useVisibleTask$(({ track }) => {
+    track(() => show);
+    if (show) {
+      console.log("iniciar temporizadon de 3 segundos");
+
+      // create interval of 3000 miliseconds
+      const interval = setInterval(() => {
+        // call onFinish function
+        props.onFinish();
+        // clear interval
+        clearInterval(interval);
+  
+      }, 3000);
+    }
+  });
+
+  return (
+    <div>
+      {show && (
+        <div class="toast toast-top toast-end mt-14">
+          <div class={typeClass}>
+            <div>
+              <span>{msg}</span>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+});
