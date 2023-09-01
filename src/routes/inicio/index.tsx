887e67a4173989146dc75a0fac686926928508f3
Marcos Iscovich <marcoscba5@gmail.com>
2023-09-01 20:36:09 -0300

    estilos dashboard

--- src/routes/inicio/index.tsx
+++ src/routes/inicio/index.tsx
@@ -2,6 +2,9 @@ import { component$, useContext } from '@builder.io/qwik';
 import { Link, type DocumentHead } from '@builder.io/qwik-city';
 import { IcoAdicionador, IcoAdmin, IcoCajero } from '../../components/router-head/iconos/iconos';
 import { AuthContext } from '~/context/auth/auth.context';
+import styles from "./style.module.css"
+import estilo from "./estilo.module.css"
+import prueba from "./prueba.module.css"
 
 export default component$(() => {
 
@@ -12,23 +15,49 @@ export default component$(() => {
 
       {authcontext.isAutenticated ? (
         <>
-          <h1>Admin 👋</h1>
           <div class="h-full flex flex-col items-center justify-center">
-            <div class="flex flex-row items-center justify-center">
+            <div class="flex flex-row items-center justify-center space-x-6">
               {authcontext.user?.rol === "Administrador" && (
-                <Link href="/administrativa" class="btn btn-lg mr-2"><IcoAdmin size={"40"} />
-                  <span class="ml-2">Adminsitrador</span>
+                <Link href="/administrativa" >
+                  {/* <span class="ml-2">Adminsitrador</span> */}
+                  <div class="body">
+                    <a class={[styles.card, styles.credentialing]} href="#">
+                      <div class={styles.overlay}></div>
+                      <div class={styles.circle}>
+                        <IcoAdmin size={"40"} />
+                      </div>
+                      <span style="font-weight: bold">Admistración</span>
+                    </a>
+                  </div>
                 </Link>
-
               )}
-
               {(authcontext.user?.rol === "Administrador" || authcontext.user?.rol === "Encargado") && (
                 <>
-                  <Link href="/adicionador" class="btn btn-lg mr-2"><IcoAdicionador size={"40"} />
-                    <span class="ml-2">Adicionador</span>
-                  </Link>
-                  <Link href="/caja" class="btn btn-lg mr-2"><IcoCajero size={"40"} />
-                    <span class="ml-2">Cajero</span>
+                  <Link href="/adicionador">
+                    <div class={estilo.cardd}>
+                      <div class={estilo.cardcontent}>
+                        <div class={estilo.cardtitle}>
+                          <div class="flex justify-center" >
+                            <IcoAdicionador size={"40"} />
+                          </div>
+                        </div>
+                        <div class="flex justify-center">
+                          <button class={estilo.cardbutton}>Adicionador</button>
+                        </div>
+
+                      </div>
+                    </div>
+                  </Link>  
+                  <Link href="/caja" >
+                    <div class={prueba.cardC}>
+                      <div class="flex justify-center">
+                           <IcoCajero size={"40"} />
+                      </div>
+                 
+                      Cajero
+                    </div>
+
+
                   </Link>
                 </>
 
@@ -39,10 +68,15 @@ export default component$(() => {
       ) :
         (
           <progress class="progress w-56"></progress>
-        )}
+        )
+      }
     </>
   );
+
+
 });
+
+
 
 export const head: DocumentHead = {
   title: 'Food-Floow',
